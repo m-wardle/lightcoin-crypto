113ccb7704cd72bb36cba7a3672ef21e698217c1
Matthew Wardle <mwardle.22@gmail.com>
2019-10-26 00:57:59 +0000

    Implement solution per activity instructions

--- index.js
+++ index.js
@@ -1,15 +1,64 @@
-let balance = 500.00;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
 
-class Withdrawal {
+  get balance() {
+    // Calculate the balance using the transaction objects
+    let balance = 0;
+    for (let item of this.transactions) {
+      balance += item.value;
+    }
+    return balance;
+  }
 
-  constructor(amount) {
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.time = new Date();
+    if (this.isAllowed() === true) {
+      this.account.addTransaction(this);
+      return true;
+    } else {
+      console.log(`${this.account.username}, you have insufficient funds to complete this transaction. Please deposit sufficient funds and try again.`);
+      return false;
+    }
   }
 
+  isAllowed() {
+    if ((this.account.balance + this.value) < 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+}
+
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
 }
 
 
@@ -17,13 +66,18 @@ class Withdrawal {
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("Charlie Lee");
 
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+console.log('Balance:', myAccount.balance);
